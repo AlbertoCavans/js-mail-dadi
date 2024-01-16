db42bad6c64e3c70a9f524c69e04b09b9f0d891b
AlbertoCavans <alberto96cavani@gmail.com>
2024-01-16 02:10:08 +0100

    Feat JS - print access

---
+++ JS/main.js
@@ -0,0 +1,32 @@
+/* LINKS TO HTML */
+const emailInput = document.getElementById("emailInput");
+const arrayMails = [
+  "pincopallo1@gmail.com",
+  "pincopallo2@gmail.com",
+  "pincopallo3@gmail.com",
+  "pincopallo4@gmail.com",
+];
+const submitData = document.getElementById("submitData");
+const accessByEmail = document.getElementById("accessByEmail");
+
+/* ACTIONS */
+submitData.addEventListener("click", function () {
+  const sentEmail = emailInput.value;
+  let verifiedMail = false;
+
+  for (let i = 0; i < arrayMails.length; i++) {
+    const observedMail = arrayMails[i];
+
+    if (sentEmail == observedMail) verifiedMail = true;
+  }
+  console.log(verifiedMail);
+
+  /*  if ((verifiedMail = false)) {
+    accessByEmail.innertext = "Verifica Email: accesso negato.";
+  } else if ((verifiedMail = true)) {
+    accessByEmail.innertext = "Verifica Email: accesso approvato.";
+  } */ /* non funziona */
+  accessByEmail.innerText = verifiedMail
+    ? "Verifica email: Accesso negato"
+    : "Verifica email: Accesso permesso";
+});
